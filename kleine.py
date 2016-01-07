c10937e2db36b1c150594d5195629154daebad63
Pierre Fenoll <pierrefenoll@gmail.com>
2016-01-07 17:09:47 +0100

    uniform

--- kleine.py
+++ kleine.py
@@ -10,11 +10,6 @@ X = np.array([ [0,0,1]
              , [1,0,1]
              , [1,1,1]
              ])
-y = np.array([ [0]
-             , [0]
-             , [1]
-             , [1]
-             ])
 
 sigmoid = lambda x: 1 / (1 + np.exp(- x))
 sigmoid_prime = lambda x: x * (1 - x)
@@ -23,28 +18,47 @@ sigmoid_prime = lambda x: x * (1 - x)
 # Seed so that each run has the same starting state
 np.random.seed(1)
 
-# Init weights randomly with mean 0
-syn0 = 2 * np.random.random((3, 1)) -1
-# syn0 = 2 * np.random.random((3, 4)) -1
-# syn1 = 2 * np.random.random((4, 1)) -1
+def nn__3_1 (l0, lminus1):
+    w0 = 2 * np.random.random((3, 1)) -1
+    for j in xrange(6 * 1000):
+        l1 = sigmoid(np.dot(l0, w0))
+        l1_delta = sigmoid_prime(l1) * (lminus1 - l1)
+        w0 += np.dot(l0.T, l1_delta)
+    print l1
 
-l0 = X
-for j in xrange(10 * 1000):
-    # Forward propagation
-    l1 = sigmoid(np.dot(l0, syn0))
+def nn__3_4_1 (l0, lminus1):
+    w0 = 2 * np.random.random((3, 4)) -1
+    w1 = 2 * np.random.random((4, 1)) -1
+    for j in xrange(10 * 1000):
+        l1 = sigmoid(np.dot(l0, w0))
+        l2 = sigmoid(np.dot(l1, w1))
+        l2_delta = sigmoid_prime(l2) * (lminus1 - l2)
+        l1_delta = sigmoid_prime(l1) * np.dot(l2_delta, w1.T)
+        w1 += np.dot(l1.T, l2_delta)
+        w0 += np.dot(l0.T, l1_delta)
+    print l2
 
-    # Back propagation
-    l1_error = y - l1
-    l1_delta = sigmoid_prime(l1) * l1_error
-    syn0 += np.dot(l0.T, l1_delta)
 
-    # #y = 0 1 1 0 .T
-    # l1 = sigmoid(np.dot(l0, syn0))
-    # l2 = sigmoid(np.dot(l1, syn1))
-    # l2_delta = sigmoid_prime(l2) * (y - l2)
-    # l1_delta = sigmoid_prime(l1) * np.dot(l2_delta, syn1.T)
-    # syn1 += np.dot(l1.T, l2_delta)
-    # syn2 += np.dot(l0.T, l1_delta)
 
-print "Layer1:"
-print l1
+y31 = np.array(
+    [ [0]
+    , [0]
+    , [1]
+    , [1]
+    ])
+
+y341 = np.array(
+    [ [0]
+    , [1]
+    , [1]
+    , [0]
+    ])
+
+
+print 31
+nn__3_1(X, y31)
+
+np.random.seed(1)
+
+print 341
+nn__3_4_1(X, y341)
